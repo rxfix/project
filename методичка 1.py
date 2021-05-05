9a52316377d1767af252390b86bae8215afc8370
rxfix <panter2004@gmail.com>
2021-05-05 18:54:29 +0300

    Merge remote-tracking branch 'origin/main' into main

--- методичка 1.py
+++ методичка 1.py
@@ -1,5 +1,19 @@
+#ВЕТВЛЕНИЯ
 number = 157
 if not number % 2: #остаток от деления на 2 равен 1, что эквивалентно true.
     print('число четное')
 else:
     print('число не четное')
+
+#СПИСКИ
+
+half_year_months = ['январь', 'февраль', 'март', 'апрель', 'июнь']
+print(type(half_year_months))
+print(half_year_months[2]) # март
+half_year_months[2] = 'fff'
+print(half_year_months[2]) # fff
+half_year_months.append('в конец')# добавление в конец списка
+print(half_year_months)#
+
+#ЦИКЛЫ
+len()
